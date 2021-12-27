4b399c1c9ffd5b28431e841a0ed6a98887b53780
pick-gyu123@naver.com <pick-gyu123@naver.com>
2021-12-27 17:19:13 +0900

    day8 - warmup 풀기

---
+++ hackerrank/warmup/compare_the_triplets.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+# Complete the 'compareTriplets' function below.
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+def compareTriplets(a, b):
+    result_a = 0
+    result_b = 0
+    temp = []
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            result_a += 1 
+        elif a[i] < b[i]:
+            result_b += 1
+    temp.append(result_a)
+    temp.append(result_b)
+    return temp
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    a = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
