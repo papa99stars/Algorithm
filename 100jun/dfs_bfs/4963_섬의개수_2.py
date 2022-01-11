099ec65fbd684331370f7cc536e336a0b79c1785
pick-gyu123@naver.com <pick-gyu123@naver.com>
2022-01-12 08:58:08 +0900

    day1

---
+++ 100jun/dfs_bfs/4963_섬의개수_2.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+read = sys.stdin.readline
+# bfs를 이용한 풀이 
+def bfs(x, y):
+  dx = [1, -1, 0, 0, 1, -1, 1, -1]
+  dy = [0, 0, -1, 1, -1, 1, 1, -1]
+  field[x][y] = 0
+  q = deque()
+  q.append([x, y])
+  while q:
+    a, b = q.popleft()
+    for i in range(8):
+      nx , ny = a + dx[i] , b + dy[i]
+      if 0 <= nx < h and 0 <= ny < w and field[nx][ny] == 1:
+        field[nx][ny] = 0
+        q.append([nx, ny])
+
+while True:
+  w, h = map(int, read().split())
+  if w == 0 and h == 0:
+    break
+  field = []
+  for _ in range(h):
+    field.append(list(map(int, read().split())))
+  count = 0
+  for i in range(h):
+    for j in range(w):
+      if field[i][j] == 1:
+        bfs(i, j)
+        count += 1
+  print(count)
